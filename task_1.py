85306d948f93609e2c445c69e1ac68838cfadc3b
bananaDundana <wedlabmo@gmail.com>
2022-12-09 17:56:53 +0300

    Task 1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,13 @@
-print('Hello')
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x1 = x
+        rev = 0
+        if x > 0:
+            while x != 0:
+                last_digit = x % 10
+                rev = rev * 10 + last_digit
+                x = x // 10
+        if rev == x1 and x1 > 0:
+            return True
+        else:
+            return False
